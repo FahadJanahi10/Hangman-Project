aca393fdf420990875a012ca0df28ef45f5e2d00
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-05 20:54:24 +0300

    not done

---
+++ script.js
@@ -0,0 +1,9 @@
+const question = document.querySelector('.question');
+const display = document.querySelector('.display');
+const lives = document.querySelector('.lives');
+const keyboard = document.querySelector('.keyboard');
+const reset = document.querySelector('.reset');
+const words = ['Chelsea', 'Arsenal', 'Liverpool']
+let letters = ;
+let remlives = ;
+const 
