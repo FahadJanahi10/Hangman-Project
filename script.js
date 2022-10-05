6061dee45f80e8402ada7bb5a18f691bfe517172
FahadJanahi10 <fahad.a.janahi@gmail.com>
2022-10-05 23:37:16 +0300

    couldnt figure out how to loop the keyboard, or how to connect it to a function

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 const question = document.querySelector('.question');
 const display = document.querySelector('.display');
+const hintDisplay = document.querySelector('.hintDisplay')
 const lives = document.querySelector('.lives');
 const keyboard = document.querySelector('.keyboard');
 const reset = document.querySelector('.reset');
-const words = ['Chelsea', 'Arsenal', 'Liverpool']
-let letters = ;
-let remlives = ;
-const 
+const hintBtn = document,querySelector('.hintBtn')
+const words = [
+    ['chelsea', 'they wear blue']
+    ['tottenham', 'they wear white']
+    ['liverpool', 'they wear red']
+    ['man city', 'they wear sky blue']
+]
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+let remLives;
+
+function startGame(){
+
+}
+
+function letterClick(){
+
+
+}
+
+function displayLetter(){
+
+}
+
+function hintClick(){
+
+}
+
+function resetClick(){
+
+}
+
+function win(){
+    
+}
